99b1e734edd22a7292ed62c9152596b5d752aa32
yaatham <110728059+yaatham@users.noreply.github.com>
2024-11-17 14:32:08 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h3>This is Home page for 9pm batch</h3>
+            </div>
+        )
+    }
+}
